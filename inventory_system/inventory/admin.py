b869fe040a25b30bd38ae55d3e3e37a5b4831247
Tshering00 <tshering.work1@gmail.com>
2025-07-11 22:07:50 +0545

    create Product and Category model

--- inventory_system/inventory/admin.py
+++ inventory_system/inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from inventory.models import Category, Product
 # Register your models here.
+
+admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name','category','quantity','price', 'created_at', 'updated_at']
+    
+admin.site.register(Product, ProductAdmin)
--- inventory_system/inventory/models.py
+++ inventory_system/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True)
+    quantity = models.PositiveIntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
