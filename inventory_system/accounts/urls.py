dc2ab2375b113122ead9bd3b7bc595a32f35959e
Tshering00 <tshering.work1@gmail.com>
2025-07-11 14:05:15 +0545

    Create user and admin dashboard

--- inventory_system/accounts/urls.py
+++ inventory_system/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views import SignupView, CusotmLoginView, CustomLogoutView
+from accounts.views import SignupView, CusotmLoginView, CustomLogoutView, UserDashboard, AdminDashboard
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', CusotmLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('dashboard/', UserDashboard.as_view(), name='user-dashboard'),
+    path('admin-dashboard/', AdminDashboard.as_view(), name='admin-dashboard'),
 ]
--- inventory_system/accounts/views.py
+++ inventory_system/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView, TemplateView
 from .forms import CustomSignupForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 class SignupView(CreateView):
@@ -12,5 +13,19 @@ class SignupView(CreateView):
 class CusotmLoginView(LoginView):
     template_name = 'accounts/login.html'
     
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return reverse_lazy("admin-dashboard")
+        return reverse_lazy('user-dashboard')
+    
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')
+    
+class UserDashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/user_dashboard.html'
+    login_url = reverse_lazy('login')
+
+class AdminDashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/admin_dashboard.html'
+    
