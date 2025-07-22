0a13abb438254e0dcb3d2fd0db1c4e5b8e63f4e7
Tshering00 <tshering.work1@gmail.com>
2025-07-22 21:57:27 +0545

    Modify home page and changed first and last name to full name in signup form

--- inventory_system/accounts/forms.py
+++ inventory_system/accounts/forms.py
@@ -3,10 +3,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomSignupForm(UserCreationForm):
-    first_name = forms.CharField(required=True, max_length=30)
-    last_name = forms.CharField(required=True, max_length=30)
+    full_name = forms.CharField(max_length=30)
     email = forms.EmailField(required=True)
     
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['full_name', 'username', 'email', 'password1', 'password2']
