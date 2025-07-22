27119082e62839deaf25160fa6036ee84254767c
Tshering00 <tshering.work1@gmail.com>
2025-07-22 13:56:27 +0545

    Create profile model

--- inventory_system/inventory_system/settings.py
+++ inventory_system/inventory_system/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'inventory', #appname
     'accounts',
     'widget_tweaks',
-    'profile',
+    'Profile',
 ]
 
 MIDDLEWARE = [
--- inventory_system/inventory_system/urls.py
+++ inventory_system/inventory_system/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("inventory.urls")),
     path('accounts/', include("accounts.urls")),
+    # path('profile/', include("profile.urls")),
 ]
