42040516d447408b91dd08de09f47c0d36b03b0b
Tshering00 <tshering.work1@gmail.com>
2025-07-29 16:36:05 +0545

    feat: Add password change feature

--- inventory_system/accounts/urls.py
+++ inventory_system/accounts/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from accounts.views import SignupView, CusotmLoginView, CustomLogoutView
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', CusotmLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    
+    path('change-password/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/change_password.html',
+        success_url=reverse_lazy('change_password_done')),
+        name="change_password"),
+    
+    path('password-changed/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/change_password_done.html'),
+        name="change_password_done"),
+    
 ]
