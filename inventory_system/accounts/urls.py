0f44e050e36a14c1a3957c1c7d346f6246277be4
Tshering00 <tshering.work1@gmail.com>
2025-07-30 13:26:04 +0545

    feat: Add password reset feature via email

--- inventory_system/accounts/urls.py
+++ inventory_system/accounts/urls.py
@@ -17,4 +17,25 @@ urlpatterns = [
         template_name='accounts/change_password_done.html'),
         name="change_password_done"),
     
+    #password reset path links
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset_form.html",
+        html_email_template_name = "accounts/password_reset_email.html",
+        success_url = "/accounts/password-reset-done/"),
+        name="password_reset" ),
+    
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"),
+        name="password_reset_done" ),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url='/accounts/password-reset-complete/'
+    ), name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
+    
+        
 ]
--- inventory_system/inventory_system/settings.py
+++ inventory_system/inventory_system/settings.py
@@ -131,3 +131,15 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#For production stage
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# EMAIL_HOST_USER = 'yourgmail@gmail.com'
+# EMAIL_HOST_PASSWORD = 'your-app-password'  # Not your actual Gmail password!
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
